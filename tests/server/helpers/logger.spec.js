bbd745a29aefecd84231b933c7254981473adce6
André Botelho Almeida <almeidaa@ae.com>
2016-09-08 17:59:22 -0300

    Adding expired token test

--- tests/server/helpers/logger.spec.js
+++ tests/server/helpers/logger.spec.js
@@ -3,6 +3,7 @@
 const should = require('chai').should();
 const jwt = require('jsonwebtoken');
 const Auth = require('../../../server/helpers/auth');
+const config = require('../../../server/config/config');
 
 describe('Auth', function () {
   it('should create a token with right info', function () {
@@ -27,6 +28,14 @@ describe('Auth', function () {
 
   it('should verify an undefined token as invalid', function () {
     let token;
+    let verified = Auth.isValidToken(token);
+
+    verified.should.be.false;
+  });
+
+  it('should verify a token 1440 secs old as invalid', function () {
+    let payload = {email: 'test@gmail.com', name: 'john doe', iat: Math.floor(Date.now() / 1000) - 1441};
+    let token = jwt.sign(payload, config.SECRET, {issuer: config.ISSUER});
     let verified = Auth.isValidToken(token);
 
     verified.should.be.false;
