e74d780f62318088d3a20330d1a43c8ceeb93ab6
André Botelho Almeida <almeidaa@ae.com>
2016-09-08 17:47:10 -0300

    Passing verify token test

--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -2,12 +2,26 @@
 
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
+const logger = require('./logger');
 
 class Auth {
   constructor() {}
 
   createToken(payload) {
     return jwt.sign(payload, config.SECRET, {issuer: config.ISSUER});
+  }
+
+  isValidToken(token) {
+    try {
+      jwt.verify(token, config.SECRET, {issuer: config.ISSUER});
+
+      return true;
+    } catch (error) {
+      logger.info('Auth: Token invalid.');
+      logger.error(error);
+
+      return false
+    }
   }
 }
 
