fbb35d79bbcb90113af31e53c6373209abae21c7
André Botelho Almeida <almeidaa@ae.com>
2016-09-14 09:35:14 -0300

    Adding ngResource

--- gulp/config.js
+++ gulp/config.js
@@ -9,7 +9,9 @@ module.exports = {
     'node_modules/angular-aria/angular-aria.min.js.map',
     'node_modules/angular-material/angular-material.min.js',
     'node_modules/angular-route/angular-route.min.js',
-    'node_modules/angular-route/angular-route.min.js.map'
+    'node_modules/angular-route/angular-route.min.js.map',
+    'node_modules/angular-resource/angular-resource.min.js',
+    'node_modules/angular-resource/angular-resource.min.js.map',
   ],
   clientCSSLib: [
     'node_modules/angular-material/angular-material.min.css'
