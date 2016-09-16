e25b7ea1641b46c0d1ad5c61e09f59482beae3cf
Andre Botelho <andrebot_almeida@hotmail.com>
2016-09-15 22:44:25 -0300

    setting env variable in package json, not in the gulp task

--- gulp/tasks/test/test-backend.js
+++ gulp/tasks/test/test-backend.js
@@ -5,7 +5,6 @@ const mocha = require('gulp-mocha');
 const gulp = require('gulp');
 
 gulp.task('test:backend', function () {
-  process.env.ENV = 'test';
   return gulp.src(config.serverTestFiles)
           .pipe(mocha());
 });
